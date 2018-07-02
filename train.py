4626ff0d6806fa8017aa5e3e2dd0bdaa4bebaf55
cclauss <cclauss@bluewin.ch>
2018-07-02 15:00:10 +0200

    import copy in train.py for line 36

--- train.py
+++ train.py
@@ -3,6 +3,7 @@
 # Licensed under the BSD 3-Clause license.
 # For full license text, see the LICENSE file in the repo root
 # or https://opensource.org/licenses/BSD-3-Clause
+import copy
 import torch
 import numpy as np
 import math
